643cf24d6b0505431c38a9d21232426d4042bcc1
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-08 11:40:19 +0300

    Added leaflet map.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,40 @@
 'use strict';
 
 angular.module('projectsApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope) {
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    let mymap = null;
+    const LOCATION = {
+      lat: 55.745737,
+      lon: 37.660808
+    };
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+    drawMap(LOCATION);
+    drawMarker(LOCATION);
+
+
+    ////////////////////////////////////////////////
+    // Implementation
+
+    function drawMap(location) {
+      mymap = L.map('my-map').setView([location.lat, location.lon], 13);
+
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYXpha2hhcm8iLCJhIjoiY2l1cDhvMnY4MDAxMzJvcGt1ZmVyaGVkYSJ9.N1F11vuDHwrrkuEXjBSOeA'
+      }).addTo(mymap);
+    }
+
+    function drawMarker(location) {
+      let marker = null;
+      if (mymap) {
+        marker = L.marker([location.lat, location.lon], {
+          title: "я здесь"
+        }).addTo(mymap);
+        marker.bindPopup("<b>Привет!</b><br>Я здесь!");
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    }
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
   });
