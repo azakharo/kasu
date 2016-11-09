778e842983033aa627e926fc809ad560c1eb416f
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-09 17:27:34 +0300

    map: added the layers control.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -66,14 +66,41 @@ angular.module('projectsApp')
       if (location) {
         mapOptions.center = [location.lat, location.lon];
       }
-      mymap = L.map($('.my-map')[0], mapOptions);
 
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      const TILE_URL_TEMPL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+      const TILE_ACCESS_TOKEN = 'pk.eyJ1IjoiYXpha2hhcm8iLCJhIjoiY2l1cDhvMnY4MDAxMzJvcGt1ZmVyaGVkYSJ9.N1F11vuDHwrrkuEXjBSOeA';
+
+      // Base layers
+      let streetTiles = L.tileLayer(TILE_URL_TEMPL, {
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoiYXpha2hhcm8iLCJhIjoiY2l1cDhvMnY4MDAxMzJvcGt1ZmVyaGVkYSJ9.N1F11vuDHwrrkuEXjBSOeA'
-      }).addTo(mymap);
+        accessToken: TILE_ACCESS_TOKEN
+        });
+      let grayscaleTiles = L.tileLayer(TILE_URL_TEMPL, {
+        maxZoom: 18,
+        id: 'mapbox.light',
+        accessToken: TILE_ACCESS_TOKEN
+      });
+
+      // Overlays
+      let mushrooms = createMushrooms();
+      let powerPoints = createPowerPoints();
+
+      // Create map
+      mapOptions.layers = [streetTiles, mushrooms, powerPoints];
+      mymap = L.map($('.my-map')[0], mapOptions);
+
+      // Layer Control
+      const baseMaps = {
+        "Карта дорог": streetTiles,
+        "Базовая карта": grayscaleTiles
+      };
+
+      var overlayMaps = {
+        "Грибные места": mushrooms,
+        "Места силы": powerPoints
+      };
+      L.control.layers(baseMaps, overlayMaps).addTo(mymap);
     }
 
     //function drawMarker(location) {
@@ -89,144 +116,134 @@ angular.module('projectsApp')
     //===========================================
     // GeoJSON layers
 
-    ////////////////////////////////////////
-    // Грибные места
-
-    const mushroomPoints = [
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Рядом с Технопарком"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [43.195033, 54.928212]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Рядом с Колхозным Рынком"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [43.375642, 54.748849]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Под Бахтызино"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [42.843923, 54.84816]
-        }
-      }
-    ];
-
-    // Mushroom icon
-    var iconMushroom = L.icon({
-      iconUrl: 'assets/images/mushroom.png',
-      iconSize:     [32, 32], // size of the icon
-      popupAnchor:  [0, -8] // point from which the popup should open relative to the iconAnchor
-    });
-
-    L.geoJSON(mushroomPoints, {
-      pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, {icon: iconMushroom}).bindPopup("<i>" + feature.properties.name + "</i>")
-          .on('mouseover', function (e) {
-            e.target.openPopup();
-          })
-          .on('mouseout', function (e) {
-            e.target.closePopup();
-          });
-      }
-    }).addTo(mymap);
-
-    // Грибные места
-    ////////////////////////////////////////
-
-
-    //**************************************
-    // Места силы
-
-    const powerPlaceStyle = {
-      "color": "#ff7800",
-      "weight": 5,
-      "opacity": 0.65
-    };
-
-    const powerPlaces = [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
+    function createMushrooms() {
+      const mushroomPoints = [
+        {
+          "type": "Feature",
           "properties": {
-            "name": "Старый Фонтан",
-            "power": "99"
+            "name": "Рядом с Технопарком"
           },
-          "coordinates": [[
-            [43.340415, 54.924501],
-            [43.339686, 54.923117],
-            [43.344578, 54.922165],
-            [43.344728, 54.922468],
-            [43.348044, 54.921837],
-            [43.348376, 54.922511],
-            [43.3452, 54.923166],
-            [43.345329, 54.923500]
-          ]]
-        }
-      },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [43.195033, 54.928212]
+          }
+        },
+        {
+          "type": "Feature",
           "properties": {
-            "name": "Лыжная база",
-            "power": "80"
+            "name": "Рядом с Колхозным Рынком"
           },
-          "coordinates": [[
-            [43.347799, 54.936358],
-            [43.348218, 54.93755],
-            [43.345139, 54.938588],
-            [43.347413, 54.940676],
-            [43.355311, 54.939341],
-            [43.358251, 54.937136],
-            [43.357693, 54.93663],
-            [43.350397, 54.936877],
-            [43.349185, 54.936067]
-          ]]
-        }
-      },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [43.375642, 54.748849]
+          }
+        },
+        {
+          "type": "Feature",
           "properties": {
-            "name": "Протяжка",
-            "power": "90"
+            "name": "Под Бахтызино"
           },
-          "coordinates": [[
-            [43.441994, 54.878620],
-            [43.440213, 54.876146],
-            [43.441066, 54.874055],
-            [43.456607, 54.869379],
-            [43.456821, 54.870641],
-            [43.452058, 54.872423],
-            [43.449826, 54.876382]
-          ]]
+          "geometry": {
+            "type": "Point",
+            "coordinates": [42.843923, 54.84816]
+          }
         }
-      }
-    ];
+      ];
 
-    L.geoJSON(powerPlaces, {style: powerPlaceStyle}).addTo(mymap);
+      // Mushroom icon
+      var iconMushroom = L.icon({
+        iconUrl: 'assets/images/mushroom.png',
+        iconSize:     [32, 32], // size of the icon
+        popupAnchor:  [0, -8] // point from which the popup should open relative to the iconAnchor
+      });
 
-    // Места силы
-    //**************************************
+      return L.geoJSON(mushroomPoints, {
+        pointToLayer: function (feature, latlng) {
+          return L.marker(latlng, {icon: iconMushroom}).bindPopup("<i>" + feature.properties.name + "</i>")
+            .on('mouseover', function (e) {
+              e.target.openPopup();
+            })
+            .on('mouseout', function (e) {
+              e.target.closePopup();
+            });
+        }
+      });
+    }
+
+    function createPowerPoints() {
+      const powerPlaceStyle = {
+        "color": "#ff7800",
+        "weight": 5,
+        "opacity": 0.65
+      };
+
+      const powerPlaces = [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Polygon",
+            "properties": {
+              "name": "Старый Фонтан",
+              "power": "99"
+            },
+            "coordinates": [[
+              [43.340415, 54.924501],
+              [43.339686, 54.923117],
+              [43.344578, 54.922165],
+              [43.344728, 54.922468],
+              [43.348044, 54.921837],
+              [43.348376, 54.922511],
+              [43.3452, 54.923166],
+              [43.345329, 54.923500]
+            ]]
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Polygon",
+            "properties": {
+              "name": "Лыжная база",
+              "power": "80"
+            },
+            "coordinates": [[
+              [43.347799, 54.936358],
+              [43.348218, 54.93755],
+              [43.345139, 54.938588],
+              [43.347413, 54.940676],
+              [43.355311, 54.939341],
+              [43.358251, 54.937136],
+              [43.357693, 54.93663],
+              [43.350397, 54.936877],
+              [43.349185, 54.936067]
+            ]]
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Polygon",
+            "properties": {
+              "name": "Протяжка",
+              "power": "90"
+            },
+            "coordinates": [[
+              [43.441994, 54.878620],
+              [43.440213, 54.876146],
+              [43.441066, 54.874055],
+              [43.456607, 54.869379],
+              [43.456821, 54.870641],
+              [43.452058, 54.872423],
+              [43.449826, 54.876382]
+            ]]
+          }
+        }
+      ];
+
+      return L.geoJSON(powerPlaces, {style: powerPlaceStyle});
+    }
 
     // GeoJSON layers
     //===========================================
-
 
   });
