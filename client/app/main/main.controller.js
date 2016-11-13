b277c8818ef56b8bb9c21b1e812f11d0731982ee
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-13 10:58:49 +0300

    Added brand animation on sidebar item click.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('projectsApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
     $scope.showSidebar = window.innerWidth >= 768;
 
     $scope.onSidebarToggleClick = function () {
@@ -18,5 +18,14 @@ angular.module('projectsApp')
         title: 'Тест'
       }
     ];
+
+    const BRAND_SELECTOR = 'a.navbar-brand > img';
+    const BRAND_ANIM_CLASSES = 'animated flipInX';
+    $scope.animateBrand = function() {
+      $(BRAND_SELECTOR).removeClass(BRAND_ANIM_CLASSES);
+      $timeout(function () {
+        $(BRAND_SELECTOR).addClass(BRAND_ANIM_CLASSES);
+      }, 0);
+    };
 
   });
