d21e406f9f79c8c96747f4dbe7331218e08625a6
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-21 10:40:40 +0300

    Rem old dead code.

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -84,10 +84,6 @@ angular.module('projectsApp')
         accessToken: TILE_ACCESS_TOKEN
       });
 
-      // Overlays
-      //let mushrooms = createMushrooms();
-      //powerPoints = createPowerPoints();
-
       // Create map
       //mapOptions.layers = [streetTiles, mushrooms, powerPoints];
       mapOptions.layers = [streetTiles];
@@ -99,19 +95,7 @@ angular.module('projectsApp')
         "Базовая карта": grayscaleTiles
       };
 
-      var overlayMaps = {
-        //"Грибные места": mushrooms,
-        //"Места силы": powerPoints
-      };
-      layersCtrl = L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-
-      // Power places info control
-      //powerInfo = createPowerPlaceInfo();
-      //powerInfo.addTo(mymap);
-
-      // Power legend
-      //let powerLegend = createPowerPlacesLegend();
-      //powerLegend.addTo(mymap);
+      layersCtrl = L.control.layers(baseMaps).addTo(mymap);
 
       $timeout(function () {
         mymap.invalidateSize();
@@ -122,245 +106,9 @@ angular.module('projectsApp')
       addAddLayerButton(mymap);
     }
 
-    //function drawMarker(location) {
-    //  let marker = null;
-    //  if (mymap) {
-    //    marker = L.marker([location.lat, location.lon], {
-    //      title: "я здесь"
-    //    }).addTo(mymap);
-    //    marker.bindPopup("<b>Привет!</b><br>Я здесь!");
-    //  }
-    //}
 
     //===========================================
     // GeoJSON layers
-
-    //function createMushrooms() {
-    //  const mushroomPoints = [
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Рядом с Технопарком"
-    //      },
-    //      "geometry": {
-    //        "type": "Point",
-    //        "coordinates": [43.195033, 54.928212]
-    //      }
-    //    },
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Рядом с Колхозным Рынком"
-    //      },
-    //      "geometry": {
-    //        "type": "Point",
-    //        "coordinates": [43.375642, 54.748849]
-    //      }
-    //    },
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Под Бахтызино"
-    //      },
-    //      "geometry": {
-    //        "type": "Point",
-    //        "coordinates": [42.843923, 54.84816]
-    //      }
-    //    }
-    //  ];
-    //
-    //  // Mushroom icon
-    //  var iconMushroom = L.icon({
-    //    iconUrl: 'assets/images/mushroom.png',
-    //    iconSize:     [32, 32], // size of the icon
-    //    popupAnchor:  [0, -8] // point from which the popup should open relative to the iconAnchor
-    //  });
-    //
-    //  return L.geoJSON(mushroomPoints, {
-    //    pointToLayer: function (feature, latlng) {
-    //      return L.marker(latlng, {icon: iconMushroom}).bindPopup("<i>" + feature.properties.name + "</i>")
-    //        .on('mouseover', function (e) {
-    //          e.target.openPopup();
-    //        })
-    //        .on('mouseout', function (e) {
-    //          e.target.closePopup();
-    //        });
-    //    }
-    //  });
-    //}
-
-
-    //////////////////////////////////////////////////
-    // Power places
-
-    //function getPowerColor(p) {
-    //  if (p < 30) {
-    //    return "#fee0d2";
-    //  }
-    //  else if (p >= 30 && p <= 60) {
-    //    return "#fc9272"
-    //  }
-    //  else {
-    //    return "#de2d26"
-    //  }
-    //}
-    //
-    //function getPowerPlaceStyle(feature) {
-    //  return {
-    //    fillColor: getPowerColor(feature.properties.power),
-    //    weight: 2,
-    //    opacity: 1,
-    //    color: 'white',
-    //    dashArray: '3',
-    //    fillOpacity: 0.7
-    //  };
-    //}
-    //
-    //function highlightPowerPlace(e) {
-    //  var layer = e.target;
-    //
-    //  layer.setStyle({
-    //    weight: 5,
-    //    color: '#666',
-    //    dashArray: '',
-    //    fillOpacity: 0.7
-    //  });
-    //
-    //  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    //    layer.bringToFront();
-    //  }
-    //
-    //  powerInfo.update(layer.feature.properties);
-    //}
-    //
-    //function resetPowerPlaces(e) {
-    //  powerPoints.resetStyle(e.target);
-    //  powerInfo.update();
-    //}
-    //
-    //function zoom2powerPlace(e) {
-    //  mymap.fitBounds(e.target.getBounds());
-    //}
-    //
-    //function onEachPowerPlace(feature, layer) {
-    //  layer.on({
-    //    mouseover: highlightPowerPlace,
-    //    mouseout: resetPowerPlaces,
-    //    click: zoom2powerPlace
-    //  });
-    //}
-    //
-    //function createPowerPoints() {
-    //  const powerPlaces = [
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Старый Фонтан",
-    //        "power": "99"
-    //      },
-    //      "geometry": {
-    //        "type": "Polygon",
-    //        "coordinates": [[
-    //          [43.340415, 54.924501],
-    //          [43.339686, 54.923117],
-    //          [43.344578, 54.922165],
-    //          [43.344728, 54.922468],
-    //          [43.348044, 54.921837],
-    //          [43.348376, 54.922511],
-    //          [43.3452, 54.923166],
-    //          [43.345329, 54.923500]
-    //        ]]
-    //      }
-    //    },
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Лыжная база",
-    //        "power": "55"
-    //      },
-    //      "geometry": {
-    //        "type": "Polygon",
-    //        "coordinates": [[
-    //          [43.347799, 54.936358],
-    //          [43.348218, 54.93755],
-    //          [43.345139, 54.938588],
-    //          [43.347413, 54.940676],
-    //          [43.355311, 54.939341],
-    //          [43.358251, 54.937136],
-    //          [43.357693, 54.93663],
-    //          [43.350397, 54.936877],
-    //          [43.349185, 54.936067]
-    //        ]]
-    //      }
-    //    },
-    //    {
-    //      "type": "Feature",
-    //      "properties": {
-    //        "name": "Протяжка",
-    //        "power": "45"
-    //      },
-    //      "geometry": {
-    //        "type": "Polygon",
-    //        "coordinates": [[
-    //          [43.441994, 54.878620],
-    //          [43.440213, 54.876146],
-    //          [43.441066, 54.874055],
-    //          [43.456607, 54.869379],
-    //          [43.456821, 54.870641],
-    //          [43.452058, 54.872423],
-    //          [43.449826, 54.876382]
-    //        ]]
-    //      }
-    //    }
-    //  ];
-    //
-    //  return L.geoJSON(powerPlaces, {
-    //    style: getPowerPlaceStyle,
-    //    onEachFeature: onEachPowerPlace
-    //  });
-    //}
-
-    //function createPowerPlaceInfo() {
-    //  let info = L.control({position: 'bottomright'});
-    //
-    //  info.onAdd = function () {
-    //    this._div = L.DomUtil.create('div', 'power-info'); // create a div with a class "info"
-    //    this.update();
-    //    return this._div;
-    //  };
-    //
-    //  // method that we will use to update the control based on feature properties passed
-    //  info.update = function (props) {
-    //    this._div.innerHTML = '<h4>Мощность места силы</h4>' +  (props ?
-    //    '<span><b>' + props.name + '</b> ' + props.power + '%</span>'
-    //      : '<span>Выберите место силы</span>');
-    //  };
-    //
-    //  return info;
-    //}
-
-    //function createPowerPlacesLegend() {
-    //  let legend = L.control({position: 'bottomright'});
-    //
-    //  legend.onAdd = function() {
-    //
-    //    var div = L.DomUtil.create('div', 'power-info power-legend'),
-    //      grades = [0, 30, 60],
-    //      labels = [];
-    //
-    //    div.innerHTML += "<b>Мощность</b><br><p></p>";
-    //    // loop through our density intervals and generate a label with a colored square for each interval
-    //    for (var i = 0; i < grades.length; i++) {
-    //      div.innerHTML +=
-    //        '<i style="background:' + getPowerColor(grades[i] + 1) + '"></i> ' +
-    //        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    //    }
-    //
-    //    return div;
-    //  };
-    //
-    //  return legend;
-    //}
 
     let allLayersData = [];
     function loadLayers(map, layersCtrl) {
@@ -421,8 +169,6 @@ angular.module('projectsApp')
       // Add button click handler
       $( "#add-layer-btn").click(function() {
         let geojson = genGeoJson();
-
-        //addLayer(`Слой ${layerNum}`, geojson, mymap, layersCtrl);
 
         let curDt = moment().format(DATETIME_FRMT);
         let layerName = `Слой ${curDt}`;
